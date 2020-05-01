96bca7ed379b7b1251dd6d050a4dbf1377128c6f
Vincent Salazar <57049365+vincesalazar@users.noreply.github.com>
2020-05-01 01:30:23 -0700

    Add files via upload

--- toDoList_app/urls.py
+++ toDoList_app/urls.py
@@ -2,17 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # TEMPLATES
     path('', views.index),
     path('homepage', views.homepage),
     path('collections', views.collectionsPage),
     path('collections/<str:title>', views.singleCollection),
-    # PROCESS
+    # LOGIN REG PROCESS
     path('register', views.register),
     path('login', views.login),
     path('logout', views.logout),
     # TASK PROCESS
-    path('createGeneralTask', views.createGenTask),
+    path('homepage/createTask', views.createGenTask),
     path('createCollection', views.createCollection),
-    path('deleteHomepageTask/<int:taskId>', views.deleteHomepageTask),
-    path('collections/delete/<int:colId>', views.deleteCollection)
+    path('createCollTask', views.createTaskForSingleCollection),
+    path('delete/homepageTask/<int:taskId>', views.deleteHomepageTask),
+    path('delete/singleColl/<int:collId>/<int:taskId>', views.deleteTaskFromSingleColl),
+    path('delete/<int:colId>', views.deleteCollFromHome),
+    path('collections/delete/<int:colId>', views.deleteCollFromColl),
 ]
