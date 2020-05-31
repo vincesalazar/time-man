6dbe969ae6a15c1b07c1c4d104a40cd115ea7e04
Vincent Salazar <57049365+vincesalazar@users.noreply.github.com>
2020-05-31 01:09:07 -0700

    Add files via upload

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -1,4 +1,4 @@
-"""toDoList URL Configuration
+"""time_man URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('', include('toDoList_app.urls'))
+    path('admin/', admin.site.urls),
+    path('', include("time_man_app.urls")),
+    path('cashtask/', include("cash_task_app.urls")),
 ]
